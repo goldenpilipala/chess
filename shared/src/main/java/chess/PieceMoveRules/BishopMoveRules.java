9e1435eb42860ed398b1d8319694e6fe0b8396a4
Beth Perkins <beth@perkcan.net>
2025-01-27 21:06:56 -0700

    Rearranging things

--- shared/src/main/java/chess/PieceMoveRules/BishopMoveRules.java
+++ shared/src/main/java/chess/PieceMoveRules/BishopMoveRules.java
@@ -7,39 +7,22 @@ import java.util.ArrayList;
 public class BishopMoveRules {
     //TODO: How to check to see if another piece is in the way
 
-    private static ArrayList<ChessMove> validMoves;
-
     public static ArrayList<ChessMove> validRules(ChessBoard board, ChessPosition original_pos) {
 
-        validMoves = new ArrayList<>();
+        ArrayList<ChessMove> validMoves = new ArrayList<>();
 
         ChessPiece.PieceType bishop = ChessPiece.PieceType.BISHOP;
 
         System.out.println("Original position: (" + original_pos.getRow() + ", " + original_pos.getColumn() + ")");
         System.out.println("Possible positions: ");
 
-        addMoves(original_pos, bishop, -1, -1);
-        addMoves(original_pos, bishop, -1, +1);
-        addMoves(original_pos, bishop, +1, -1);
-        addMoves(original_pos, bishop, +1, +1);
-
+        MoveRules.addMoves(original_pos, bishop, validMoves, -1, -1);
+        MoveRules.addMoves(original_pos, bishop, validMoves, -1, +1);
+        MoveRules.addMoves(original_pos, bishop, validMoves, +1, -1);
+        MoveRules.addMoves(original_pos, bishop, validMoves, +1, +1);
 
         return validMoves;
     }
 
-    private static void addMoves(ChessPosition original_pos, ChessPiece.PieceType piece, int colShift, int rowShift){
-        int curr_x = original_pos.getColumn();
-        int curr_y = original_pos.getRow();
-        while(true){
-            curr_x += colShift;
-            curr_y += rowShift;
-
-            if(MoveRules.isOnBoard(curr_y, curr_x) && !MoveRules.isBlocked(curr_y, curr_x)) {
-                validMoves.add(new ChessMove(original_pos, new ChessPosition(curr_y, curr_x), piece));
-                System.out.println("(" + curr_y + ", " + curr_x + ")");
-            }
-            else break;
-        }
-    }
 
 }
--- shared/src/main/java/chess/PieceMoveRules/MoveRules.java
+++ shared/src/main/java/chess/PieceMoveRules/MoveRules.java
@@ -1,6 +1,28 @@
 package chess.PieceMoveRules;
 
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+
 public class MoveRules {
+
+    public static void addMoves(ChessPosition original_pos, ChessPiece.PieceType piece,
+                                 ArrayList<ChessMove> validMoves, int colShift, int rowShift){
+        int curr_x = original_pos.getColumn();
+        int curr_y = original_pos.getRow();
+        while(true){
+            curr_x += colShift;
+            curr_y += rowShift;
+
+            if(MoveRules.isOnBoard(curr_y, curr_x) && !MoveRules.isBlocked(curr_y, curr_x)) {
+                validMoves.add(new ChessMove(original_pos, new ChessPosition(curr_y, curr_x), piece));
+                System.out.println("(" + curr_y + ", " + curr_x + ")");
+            }
+            else break;
+        }
+    }
 
     public static boolean isOnBoard(int y, int x) {
         return x >= 1 && y >= 1 && x <= 8 && y <= 8;
